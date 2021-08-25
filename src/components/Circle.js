758000e017b00b11b06fc0cb795c15bccf6fb8df
ashishprasad2163 <ashishprasad2163@gmail.com>
2021-08-25 21:37:47 +0530

    spring animation

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import React,{ useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Animated } from 'react-native'
 
 const Circle = () => {
+    const position = useRef(new Animated.ValueXY(0,0)).current;
+    useEffect(() => {
+        Animated.spring(position,{
+            toValue: { x: 200 , y : 500},
+            useNativeDriver: false,
+        }).start();
+    },[position]);
     return(
         <View>
             <Text>
                 Circle
             </Text>
+            <Animated.View style={position.getLayout()}>
             <View style={styles.ball}/>
+            </Animated.View>
         </View>
     )
 };
